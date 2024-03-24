db816464266da863dcef6d0a99f778aa68c9e32a
brensophab <85639596+brensophab@users.noreply.github.com>
2024-03-24 16:09:30 -0500

    added backend stuff hopefully

---
+++ website_sd/backend/config.js
@@ -0,0 +1,2 @@
+module.exports.PORT = 5555;
+module.exports.mongoDBURL = "mongodb+srv://bren:bren@cluster0.rccirwe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
---
+++ website_sd/backend/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const { mongoDBURL, PORT } = require('./config') ;
+
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+mongoose
+  .connect(mongoDBURL, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
