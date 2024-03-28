dc1771df969c9dc04d418822134482a607d2caf9
brensophab <85639596+brensophab@users.noreply.github.com>
2024-03-28 17:11:03 -0500

    connected front end and backend login logic with no css

--- website_sd/frontend/src/App.js
+++ website_sd/frontend/src/App.js
@@ -1,19 +1,20 @@
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
+import  Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 import { Route, Routes } from "react-router-dom";
 import React from 'react'
 import './index.css'
-import CreateAccount from "./pages/CreateAccount";
-import Reports from "./pages/Reports";
-import Dashboard from "./pages/Dashboard";
-import Error from "./pages/Error";
-import Login from "./pages/Login";
-
+import CreateAccount from "./pages/CreateAccount.jsx";
+import Reports from "./pages/Reports.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Error from "./pages/Error.jsx";
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path="/create-account" element={<CreateAccount/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
       <Route path="/reports" element={<Reports/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/error" element={<Error/>}/>
--- website_sd/frontend/src/index.jsx
+++ website_sd/frontend/src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import './index.css';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //Remove the React.StrictMode later when you are testing the application and your data is being fetched twice
+  <React.StrictMode>
   <BrowserRouter>
       <App />
   </BrowserRouter>
+  </React.StrictMode>
 );
